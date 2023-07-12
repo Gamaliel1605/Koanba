0caec14580904918479b8fc4635a36cec0de8fd6
CarlosGamaliel <carlos.gamaliel@misteraladin.com>
2023-07-12 15:31:38 +0700

    setup app delegate and remove scene delegate

---
+++ Koanba/App/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Koanba
+//
+//  Created by Carlos Gamaliel on 12/07/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var mainNavigationController: UINavigationController?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let homeVC = HomeViewController()
+        
+        mainNavigationController = UINavigationController(rootViewController: homeVC)
+        mainNavigationController?.isNavigationBarHidden = false
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = mainNavigationController
+        
+        return true
+    }
+
+}
+
